c1d54a43da3a4a5ff546ba01c6c8867503773fe6
Nghia Nguyen <zinzinday@gmail.com>
2013-09-13 07:09:26 +0700

    WIP

---
+++ yii/markitup/MarkItUp.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\markitup;
+
+use yii\widgets\InputWidget;
+
+/**
+ * @author Nghia Nguyen <yiidevelop@hotmail.com>
+ * @since 2.0
+ */
+class MarkItUp extends InputWidget
+{
+    public function init()
+    {
+        
+    }
+
+    public function run()
+    {
+        
+    }
+
+}
